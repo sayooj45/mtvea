81475c8b1b63e31f4ba2f8d08b8e33e62f5e24fa
sayooj45 <sayooj4545@gmail.com>
2026-03-21 19:56:56 +0530

    vercel isssue

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  base: '/', // ensures all routes resolve correctly on refresh
+  plugins: [
+    react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist', // default is dist, make sure matches Vercel settings
+  }
 })
