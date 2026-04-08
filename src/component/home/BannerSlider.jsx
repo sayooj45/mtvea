a723df42a9fe5a48dbf7e7ce3885fda7db40d784
sayooj45 <sayooj4545@gmail.com>
2026-04-08 14:53:49 +0530

    commit

--- src/component/home/BannerSlider.jsx
+++ src/component/home/BannerSlider.jsx
@@ -254,7 +254,7 @@ const BannerSlider = () => {
                 onClick={() => navigate("/speakers")}
                 className="group cursor-pointer bg-white/10 backdrop-blur-md rounded-xl overflow-hidden border border-white/20 shadow-lg transition duration-300 hover:scale-105 hover:shadow-xl flex flex-col"
               >
-                <div className="w-full aspect-[4/3.7] sm:aspect-[4/3] md:aspect-[4/4] bg-[#1B2B4B] overflow-hidden shrink-0 ">
+                <div className="w-full aspect-[4/3] sm:aspect-[4/3] md:aspect-[4/4] bg-[#1B2B4B] overflow-hidden shrink-0 ">
                   <img
                     src={speaker1}
                     alt="Rt. Rev. Dr. Abraham Mar Paulos Episcopa"
@@ -275,7 +275,7 @@ const BannerSlider = () => {
                 onClick={() => navigate("/speakers")}
                 className="group cursor-pointer bg-white/10 backdrop-blur-md rounded-xl overflow-hidden border border-white/20 shadow-lg transition duration-300 hover:scale-105 hover:shadow-xl"
               >
-                <div className="w-full aspect-[4/3.7] sm:aspect-[4/3] md:aspect-[4/4] bg-[#1B2B4B] overflow-hidden shrink-0">
+                <div className="w-full aspect-[4/3] sm:aspect-[4/3] md:aspect-[4/4] bg-[#1B2B4B] overflow-hidden shrink-0">
                   <img
                     src={speaker2}
                     alt="Rev. Mothy Varkey"
