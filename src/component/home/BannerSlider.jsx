00bb9c608132dc6376fe10946fa10a4259b1c2bf
sayooj45 <sayooj4545@gmail.com>
2026-04-08 14:04:18 +0530

    commit

--- src/component/home/BannerSlider.jsx
+++ src/component/home/BannerSlider.jsx
@@ -54,7 +54,7 @@ const BannerSlider = () => {
   };
 
   return (
-    <div className="relative min-h-[calc(100vh-20px)] overflow-hidden">
+    <div className="relative min-h-[calc(100vh-100px)] overflow-hidden">
       {/* Background Slider */}
       {slides.map((img, i) => (
         <div
